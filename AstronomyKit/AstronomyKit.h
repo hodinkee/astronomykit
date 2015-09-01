f4a17d0296c686659776626f853e03ab5e002b13
Caleb Davenport <caleb@calebd.me>
2015-09-01 16:31:54 -0400

    Add empty Xcode project.

---
+++ AstronomyKit/AstronomyKit.h
@@ -0,0 +1,19 @@
+//
+//  AstronomyKit.h
+//  AstronomyKit
+//
+//  Created by Caleb Davenport on 9/1/15.
+//  Copyright (c) 2015 Hodinkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AstronomyKit.
+FOUNDATION_EXPORT double AstronomyKitVersionNumber;
+
+//! Project version string for AstronomyKit.
+FOUNDATION_EXPORT const unsigned char AstronomyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AstronomyKit/PublicHeader.h>
+
+
